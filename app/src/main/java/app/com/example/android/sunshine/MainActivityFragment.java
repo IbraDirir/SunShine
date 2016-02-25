67edbb19b7f7950e332c83c2fe4ec4b81b1eea13
IbraDirir <khaliildirir@gmail.com>
2016-02-25 12:52:21 +0300

    added comments to the code

--- app/src/main/java/app/com/example/android/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/android/sunshine/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForcastAdapter;
 
     public MainActivityFragment() {
 
@@ -32,8 +35,28 @@ public class MainActivityFragment extends Fragment {
 
         };
 
+
         List<String> weekForcast = new ArrayList<>(
         Arrays.asList(forecastArray));
+
+
+      //Now that we have some dummy forcast data, create an arrrayAdapter.
+      //The ArrayAddapter will take data from a source (like our dummy forcast)
+      //use it to populate the listView it's attached to
+       mForcastAdapter =
+                new ArrayAdapter<String>(
+                        //the current context(this fragment's parent activity )
+                        getActivity(),
+                        //ID of the list item layout
+                R.layout.list_item_forecast,
+                        //ID of the textView to populate
+                        R.id.list_item_forecast_text_view,
+                        //forcast Data
+                        weekForcast );
+
+
+
+
 
 
         return inflater.inflate(R.layout.fragment_main, container, false);
